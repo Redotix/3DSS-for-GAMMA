ca0d51fa5c0aba3ab5219e9a5c91f68ba61be451
Redotix <hlivakmatej@gmail.com>
2024-09-08 02:08:13 +0200

    Stencil Testing, NVG Blur, SSS Hud Mask Fix

--- 3DSS for GAMMA/gamedata/shaders/r3/models_scope_back.s
+++ 3DSS for GAMMA/gamedata/shaders/r3/models_scope_back.s
@@ -5,6 +5,6 @@ function normal(shader, t_base, t_second, t_detail)
     : scopelense(1)
     shader:dx10texture("s_base", t_base)
     shader:dx10sampler("smp_base")
-    shader:dx10stencil(true, cmp_func.always, 255, 127, stencil_op.keep, stencil_op.keep, stencil_op.keep)
-    shader:dx10stencil_ref(1)
+    shader:dx10stencil(true, cmp_func.always, 2, 2, stencil_op.replace, stencil_op.replace, stencil_op.replace)
+    shader:dx10stencil_ref(2)
 end
